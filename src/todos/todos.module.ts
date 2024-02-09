4f7afe5df7b140c2ef847cd963f92a2541393dae
burakbehlull <burakyabgu@gmail.com>
2024-02-09 03:29:00 +0300

    feat: Installed MongoDB and configured settings

---
+++ src/todos/todos.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
+import {TodosController} from './todos.controller'
+import {TodosService} from './todos.service'
+@Module({
+    imports: [
+        MongooseModule.forFeature([{ name: 'Todos', schema: TodosSchema }])
+    ],
+    controllers: [TodosController],
+    providers: [TodosService],
+})
+export class TodosModule {}
